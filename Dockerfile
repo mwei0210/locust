e61c860ecb78deb08640d3680ef3aa8db433c3e0
ming <mwei0210@gmail.com>
2020-05-20 09:48:36 +0800

    bump locust version to 1.0.1

--- Dockerfile
+++ Dockerfile
@@ -8,13 +8,15 @@ RUN pip install .
 FROM python:3.8-alpine
 
 RUN apk --no-cache add zeromq curl && adduser -s /bin/false -D locust
-COPY --from=builder /usr/local/lib/python3.6/site-packages /usr/local/lib/python3.6/site-packages
+COPY --from=builder /usr/local/lib/python3.8/site-packages /usr/local/lib/python3.8/site-packages
 COPY --from=builder /usr/local/bin/locust /usr/local/bin/locust
+COPY docker_start.sh docker_start.sh
+RUN chmod +x docker_start.sh
 
 EXPOSE 8089 5557
 
 USER locust
-ENTRYPOINT ["locust"]
+CMD ["./docker_start.sh"]
 
 # turn off python output buffering
 ENV PYTHONUNBUFFERED=1
--- docker_start.sh
+++ docker_start.sh
@@ -1,38 +1,18 @@
 #!/usr/bin/env sh
 
-#if [ -z "${TARGET_URL}" ]; then
-#  echo "ERROR: TARGET_URL not configured" >&2
-#  exit 1
-#fi
-
-LOCUST_MODE="${LOCUST_MODE:=standalone}"
 LOCUST_SCRIPT_DIR_PATH="${LOCUST_SCRIPT_DIR_PATH:-/locust}"
-_LOCUST_OPTS="-f ${LOCUSTFILE_PATH:-$LOCUST_SCRIPT_DIR_PATH/locustfile.py}"
-REQUIREMENT_FILE_NAME="${REQUIREMENT_FILE_NAME:-requirements.txt}"
+REQUIREMENT_FILE_PATH="${REQUIREMENT_FILE_PATH:-$LOCUST_SCRIPT_DIR_PATH/requirements.txt}"
 
-if [ -f "$file" ]; then
-    echo "$file found. Installing dependencies..."
-    pip install --user locust -r $LOCUST_SCRIPT_DIR_PATH/$REQUIREMENT_FILE_NAME
+if [ -f "$REQUIREMENT_FILE_PATH" ]; then
+    echo "$REQUIREMENT_FILE_PATH found. Installing dependencies..."
+    pip install --user locust -r $REQUIREMENT_FILE_PATH
 else
-    echo "$file not found."
+    echo "$REQUIREMENT_FILE_PATH not found."
 fi
 
-if [ "${LOCUST_MODE}" = "MASTER" ]; then
-    _LOCUST_OPTS="${_LOCUST_OPTS} --master"
-elif [ "${LOCUST_MODE}" = "SLAVE" ]; then
-    if [ -z "${LOCUST_MASTER_HOST}" ]; then
-        echo "ERROR: MASTER_HOST is empty. Slave mode requires a master" >&2
-        exit 1
-    fi
-
-    _LOCUST_OPTS="${_LOCUST_OPTS} --slave --master-host=${LOCUST_MASTER_HOST} --master-port=${LOCUST_MASTER_PORT:-5557}"
-fi
-
-echo "Starting Locust in ${LOCUST_MODE} mode..."
-echo "$ locust ${LOCUST_OPTS} ${_LOCUST_OPTS}"
-cd /locust
-ls -al
 if [ -n "$RUN_SCRIPT" ]; then
+    echo "running $RUN_SCRIPT..."
     $RUN_SCRIPT &
 fi
-exec locust ${LOCUST_OPTS} ${_LOCUST_OPTS}
+
+exec locust
