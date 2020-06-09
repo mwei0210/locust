fdff85ae9bb9fa1fb4c41e84378cb9e5c379573f
ming <mwei0210@gmail.com>
2020-06-09 14:26:52 +0800

    custom script

--- docker_start.sh
+++ docker_start.sh
@@ -1,16 +1,25 @@
 #!/usr/bin/env sh
 
-if [ -z "${TARGET_URL}" ]; then
-  echo "ERROR: TARGET_URL not configured" >&2
-  exit 1
-fi
+#if [ -z "${TARGET_URL}" ]; then
+#  echo "ERROR: TARGET_URL not configured" >&2
+#  exit 1
+#fi
 
 LOCUST_MODE="${LOCUST_MODE:=standalone}"
-_LOCUST_OPTS="-f ${LOCUSTFILE_PATH:-/locustfile.py} -H ${TARGET_URL}"
+LOCUST_SCRIPT_DIR_PATH="${LOCUST_SCRIPT_DIR_PATH:-/locust}"
+_LOCUST_OPTS="-f ${LOCUSTFILE_PATH:-$LOCUST_SCRIPT_DIR_PATH/locustfile.py}"
+REQUIREMENT_FILE_NAME="${REQUIREMENT_FILE_NAME:-$LOCUST_SCRIPT_DIR_PATH/requirements.txt}"
 
-if [ "${LOCUST_MODE}" = "master" ]; then
+if [ -f "$file" ]; then
+    echo "$file found. Installing dependencies..."
+    pip install --user locust -r $LOCUST_SCRIPT_DIR_PATH/$REQUIREMENT_FILE_NAME
+else
+    echo "$file not found."
+fi
+
+if [ "${LOCUST_MODE}" = "MASTER" ]; then
     _LOCUST_OPTS="${_LOCUST_OPTS} --master"
-elif [ "${LOCUST_MODE}" = "slave" ]; then
+elif [ "${LOCUST_MODE}" = "SLAVE" ]; then
     if [ -z "${LOCUST_MASTER_HOST}" ]; then
         echo "ERROR: MASTER_HOST is empty. Slave mode requires a master" >&2
         exit 1
@@ -21,5 +30,9 @@ fi
 
 echo "Starting Locust in ${LOCUST_MODE} mode..."
 echo "$ locust ${LOCUST_OPTS} ${_LOCUST_OPTS}"
-
+cd /locust
+ls -al
+if [ -n "$RUN_SCRIPT" ]; then
+    $RUN_SCRIPT &
+fi
 exec locust ${LOCUST_OPTS} ${_LOCUST_OPTS}
